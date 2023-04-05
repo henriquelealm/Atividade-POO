bb0b9ca5f3081d49511817be725c52c13daee9ae
Gabriel Galdino <gabrieljgaldino22@gmail.com>
2023-04-05 10:12:55 -0300

    Mediator

--- br/gov/cesarschool/poo/fidelidade/cliente/negocio/ClienteMediator.java
+++ br/gov/cesarschool/poo/fidelidade/cliente/negocio/ClienteMediator.java
@@ -1,10 +1,15 @@
 package br.gov.cesarschool.poo.fidelidade.cliente.negocio;
 
 import br.gov.cesarschool.poo.fidelidade.util.*;
+import br.gov.cesarschool.poo.fidelidade.geral.entidade.Endereco;
 import br.gov.cesarschool.poo.fidelidade.cliente.dao.ClienteDAO;
 import br.gov.cesarschool.poo.fidelidade.cartao.negocio.CartaoFidelidadeMediator;
 import br.gov.cesarschool.poo.fidelidade.cliente.entidade.Cliente;
 import br.gov.cesarschool.poo.fidelidade.cartao.dao.LancamentoExtratoDAO;
+
+import javax.security.sasl.SaslException;
+import java.util.Date;
+
 import br.gov.cesarschool.poo.fidelidade.cartao.dao.CartaoFidelidadeDAO;
 import br.gov.cesarschool.poo.fidelidade.util.ResultadoInclusaoCliente;
 
@@ -37,9 +42,45 @@ public class ClienteMediator {
     private String validar(Cliente cliente){
         if (!ValidadorCPF.ehCpfValido(cliente.getCpf())){
             return "CPF Inválido";
-        }else{
-            return null;
         }
+        if (!StringUtil.ehNuloOuBranco(cliente.getNomeCompleto())){
+            return "Nome Inválido";
+        }
+        if (!StringUtil.ehNuloOuBranco(cliente.getSexo().getDescricao())){
+            return "Sexo não selecionado";
+        }
+        int idadeAtual = cliente.obterIdade();
+        if (idadeAtual < 18 && idadeAtual > 0){
+            return "Menor de idade";
+        }
+        if (idadeAtual == 0){
+            return "Idade não inserida";
+        }
+        if (cliente.getRenda() < 0){
+            return "Renda nao pode ser negativa";
+        }
+        Endereco endereco = cliente.getEndereco();
+        if(endereco == null){
+            return "campo 'Endereco' obrigatorio";
+        }else{
+            String logradouro = endereco.getLogradouro();
+            if(!StringUtil.ehNuloOuBranco(logradouro) || logradouro.length() < 4){
+                return "campo 'Logradouro' obrigatorio e deve ter 4 caracteres";
+            }
+            if(endereco.getNumero() < 0){
+                return "Numero nao pode ser negativo";
+            }
+            if (!StringUtil.ehNuloOuBranco(endereco.getCidade())){
+                return "campo 'Cidade' obrigatorio";
+            }
+            if (!StringUtil.ehNuloOuBranco(endereco.getEstado())){
+                return "campo 'Estado' obrigatorio";
+            }
+            if (!StringUtil.ehNuloOuBranco(endereco.getPais())){
+                return "campo 'Pais' obrigatorio";
+            }
+        }return "Dados validos";
+
     }
 
     public ResultadoInclusaoCliente incluir(Cliente cliente){
